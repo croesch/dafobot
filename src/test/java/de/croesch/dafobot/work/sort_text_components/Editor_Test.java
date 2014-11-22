6111a10fb9603d198419d5c69d06281b5d576c28
Christian Rösch <christianroesch@gmx.net>
2014-11-22 15:34:09 +0100

    Add test case.

--- src/test/java/de/croesch/dafobot/work/sort_text_components/Editor_Test.java
+++ src/test/java/de/croesch/dafobot/work/sort_text_components/Editor_Test.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.io.IOException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
@@ -21,15 +22,18 @@ public class Editor_Test extends Editor_TestCase {
 
   @Test
   public void should_Edit_Page_So_That_Result_Equals_Result_File() throws IOException {
-    for (final Path path : allPathFiles("/sort_text_components/beforeafter/")) {
+    final List<Path> testFiles = allPathFiles("/sort_text_components/beforeafter/");
+    for (final Path path : testFiles) {
       final String result = new Editor().doSpecialEdit(path.toString(), stringOf(path));
       assertThat(result).as(path.toString()).isEqualTo(stringOf(afterPath(path)));
     }
+    System.out.println("SUCCES testing " + testFiles.size() + " test files.");
   }
 
   @Test
   public void should_Throw_NoEditNeededException_For_These_Articles() throws IOException {
-    for (final Path path : allPathFiles("/sort_text_components/noeditneeded/")) {
+    final List<Path> testFiles = allPathFiles("/sort_text_components/noeditneeded/");
+    for (final Path path : testFiles) {
       try {
         new Editor().doSpecialEdit(path.toString(), stringOf(path));
         Assertions.fail("Didn't throw exception for: " + path.toString());
@@ -37,6 +41,7 @@ public class Editor_Test extends Editor_TestCase {
         // good!
       }
     }
+    System.out.println("SUCCES testing " + testFiles.size() + " test files.");
   }
 
   private Path afterPath(final Path path) {
